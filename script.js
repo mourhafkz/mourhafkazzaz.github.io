c058ac19ae81fc69f2bec82b9e938ca4259ccc58
mourhafkz <mourhafkazzaz@gmail.com>
2023-04-14 01:01:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 $(document).ready(function () {
+    function convert(obj) {
+        return Object.keys(obj).map(key => ({
+            field: key,
+            value: obj[key],
+        }));
+    }
 
-    $("#article_question, #phrase_question, #word_question").click(function (event) {
+
+    $("#article_question, #phrase_question, #word_question, #conjugate_question").click(function (event) {
 
         filename = $(this).attr('id').split('_')[0] + 's.json'  //article_question to articles.json
 
         $.getJSON('data/' + filename, function (data) {
-
+            // random word
             var random_entry = data.data[Math.floor(Math.random() * data.data.length)]
-            if (random_entry.status == 0) {
-                $('#front').html('<p>' + random_entry.task + '</p>');
-                $('#back').html('<p>' + random_entry.solution + '</p>');
+
+            if (filename == "conjugates.json") {
+                // random pronoun from conjugation table
+                new_obj = convert(random_entry)
+                var random_key = new_obj[Math.floor(Math.random() * new_obj.length)]
+                console.log(random_key.field);
+                if (random_key.field != "verb") {
+                    $('#front').html('<p>' + random_key.field + ' _________</p><p class="tense_display">(' + random_entry.verb + ')</p>');
+                    $('#back').html('<p>' + random_key.field + ' ' + random_key.value + '</p>');
+                }
+
             } else {
                 $('#front').html('<p>' + random_entry.task + '</p>');
                 $('#back').html('<p>' + random_entry.solution + '</p>');
             }
+
+
 
         })
 
