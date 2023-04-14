08b19dc320f83c5b4262952637b55dbfd1321656
mourhafkz <mourhafkazzaz@gmail.com>
2023-04-14 13:25:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -14,22 +14,22 @@ $(document).ready(function () {
         $.getJSON('data/' + filename, function (data) {
             // random word
             var random_entry = data.data[Math.floor(Math.random() * data.data.length)]
+            if (random_entry.status == 0) {
+                if (filename == "conjugates.json") {
+                    // random pronoun from conjugation table
+                    new_obj = convert(random_entry)
+                    var random_key = new_obj[Math.floor(Math.random() * new_obj.length)]
+                    console.log(random_key.field);
+                    if (random_key.field != "verb" && random_key.field != "status") {
+                        $('#front').html('<p>' + random_key.field + ' _________</p><p class="tense_display">(' + random_entry.verb + ')</p>');
+                        $('#back').html('<p>' + random_key.field + ' ' + random_key.value + '</p>');
+                    }
 
-            if (filename == "conjugates.json") {
-                // random pronoun from conjugation table
-                new_obj = convert(random_entry)
-                var random_key = new_obj[Math.floor(Math.random() * new_obj.length)]
-                console.log(random_key.field);
-                if (random_key.field != "verb") {
-                    $('#front').html('<p>' + random_key.field + ' _________</p><p class="tense_display">(' + random_entry.verb + ')</p>');
-                    $('#back').html('<p>' + random_key.field + ' ' + random_key.value + '</p>');
+                } else {
+                    $('#front').html('<p>' + random_entry.task + '</p>');
+                    $('#back').html('<p>' + random_entry.solution + '</p>');
                 }
-
-            } else {
-                $('#front').html('<p>' + random_entry.task + '</p>');
-                $('#back').html('<p>' + random_entry.solution + '</p>');
             }
-
 
 
         })
